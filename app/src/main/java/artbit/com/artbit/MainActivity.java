e8d581b795ec70a6bfc3f5913f822372330e3502
wjstjsgml147 <wjstjsgml147@naver.com>
2016-12-19 21:27:46 +0900

    아트비트

---
+++ app/src/main/java/artbit/com/artbit/MainActivity.java
@@ -0,0 +1,17 @@
+package artbit.com.artbit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JoenSeonHee on 2016-12-19.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
